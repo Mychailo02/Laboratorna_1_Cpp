b7e4481d612f1cc523a8ccda89dab0b62f3268f3
Михаил Бурков <mikhail.burkov2003@gmail.com>
2020-10-27 11:45:08 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,20 @@
+﻿// ConsoleApplication6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+
+using namespace std;
+int main() {
+    float b, q;
+    float s;
+    setlocale(LC_CTYPE, "ukr");
+    cout << "Введiть перший член: ";
+    cin >> b;
+
+    cout << "Введiть знаменник, по модулю менший один: ";
+    cin >> q;
+
+    s = b / (1 - q);
+    cout << s << "\n";
+}
